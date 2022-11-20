72f1d5df378e383c3d106fb4c1118d762e3d1833
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2022-11-20 16:09:10 -0400

    Edição do arquivo README, proposito da aplicação

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const app = express()
 //app.use(logger("tiny"));
 //app.use(routes)
 app.get("/", (req, res) => {
-    res.send("Hello world miranda2")
+    res.send("Hello world")
 });
 
 app.listen(PORT, () => {
