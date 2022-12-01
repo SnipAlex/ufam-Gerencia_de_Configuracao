f39df6c72c37e505de6f30d589fcd9df9204d126
Alexandre F <alexandre36viieira@gmail.com>
2022-12-01 17:43:51 -0400

    Adic basicas do CRUD

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,9 +1,9 @@
 import express from "express"
+import todoController from "../controllers/todo"
 
 const router = express.Router()
 
-router.get("/", (req, res) =>{
-    res.send("Hello World Back");
-});
+router.get("/", todoController.index);
+router.post("/", todoController.create);
 
 export default router;
