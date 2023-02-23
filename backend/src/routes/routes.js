37a3a3278a1a18310cf68505fb07d3810d942b3a
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2023-02-23 16:12:30 -0400

    correção completa

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -3,11 +3,11 @@ const todoController = require('../controllers/todo')
 
 const router = express.Router()
 
-router.get("/", todoController.index);
-router.post("/", todoController.create);
-router.get("/:id", todoController.read);
-router.post("/:id", todoController.update);
-router.path("/:id", todoController.setAsDone);
-router.delete("/:id", todoController.remove);
+router.get("/todo", todoController.index);
+router.post("/create", todoController.create);
+router.get("/read/:id", todoController.read);
+router.put("/update/:id", todoController.update);
+router.patch("/Done/:id", todoController.setAsDone);
+router.delete("/remove/:id", todoController.remove);
 
 module.exports = router;
